ecd9c859a27fcaecd6c2cc97b25c0b82584c72e4
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-27 19:03:43 +0600

    game info

--- core/src/helpers/GameInfo.java
+++ core/src/helpers/GameInfo.java
@@ -4,5 +4,6 @@ package helpers;
  * Created by Shah Md Imran Hossain on 27, September, 2020
  */
 public class GameInfo {
-    
+    public static int width = 480;
+    public static int height = 800;
 }
