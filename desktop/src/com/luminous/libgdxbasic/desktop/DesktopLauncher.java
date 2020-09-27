0e9d6a0bb87bc4805acc6faa0f4d94a25817e927
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-27 19:45:35 +0600

    desktop

--- desktop/src/com/luminous/libgdxbasic/desktop/DesktopLauncher.java
+++ desktop/src/com/luminous/libgdxbasic/desktop/DesktopLauncher.java
@@ -1,15 +1,18 @@
 package com.luminous.libgdxbasic.desktop;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.luminous.libgdxbasic.GameMain;
+
+import helpers.GameInfo;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.width = 480;
-		config.height = 800;
+		config.width = GameInfo.WIDTH;
+		config.height = GameInfo.HEIGHT;
 
 		new LwjglApplication(new GameMain(), config);
 	}
