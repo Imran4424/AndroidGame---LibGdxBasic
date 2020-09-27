66d278ecc65263e82516530d4faf1e51d541e7ce
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-27 20:20:03 +0600

    image

--- core/src/com/luminous/libgdxbasic/GameMain.java
+++ core/src/com/luminous/libgdxbasic/GameMain.java
@@ -1,6 +1,5 @@
 package com.luminous.libgdxbasic;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -8,18 +7,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import helpers.GameInfo;
-import sun.security.timestamp.TSRequest;
 
 public class GameMain extends Game {
 	SpriteBatch batch;
 	Texture img;
-	Texture img2;
+	Texture player;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("Game BG.png");
-		img2 = new Texture("badlogic.jpg");
+		player = new Texture("Player 1.png");
 	}
 
 	@Override
@@ -28,7 +26,7 @@ public class GameMain extends Game {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
-		batch.draw(img2, GameInfo.WIDTH /2 - img2.getWidth() / 2, GameInfo.HEIGHT/2 - img2.getHeight() / 2);
+		batch.draw(player, GameInfo.WIDTH /2 - player.getWidth() / 2, GameInfo.HEIGHT/2 - player.getHeight() / 2);
 		batch.end();
 	}
 	
