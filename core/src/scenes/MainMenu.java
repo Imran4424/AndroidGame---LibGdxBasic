6a1ca7a6c4e8cbdc948fcca4e64896ab848f03d7
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-03 20:56:04 +0600

    scenes

--- core/src/scenes/MainMenu.java
+++ core/src/scenes/MainMenu.java
@@ -1,8 +1,44 @@
 package scenes;
 
+import com.badlogic.gdx.Screen;
+
 /**
  * Created by Shah Md Imran Hossain on 03, October, 2020
  */
-public class MainMenu {
+public class MainMenu implements Screen {
 
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
