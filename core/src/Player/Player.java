b7bbee6bc9dd22877adbdb51b5ca9c3f508bc348
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-04 14:23:40 +0600

    scenes

--- core/src/Player/Player.java
+++ core/src/Player/Player.java
@@ -1,9 +1,14 @@
 package Player;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 /**
  * Created by Shah Md Imran Hossain on 04, October, 2020
  */
 
-public class Player {
+public class Player extends Sprite {
 
+    public Player(String name, float x, float y) {
+        
+    }
 }
