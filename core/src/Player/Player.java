4cdb389be4b50ab2fd880857dcddf8585737341e
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-11 23:01:03 +0600

    game physics

--- core/src/Player/Player.java
+++ core/src/Player/Player.java
@@ -2,12 +2,16 @@ package Player;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Created by Shah Md Imran Hossain on 04, October, 2020
  */
 
 public class Player extends Sprite {
+    private World world;
+    private Body body;
 
     public Player(String name, float x, float y) {
         super(new Texture(name));
