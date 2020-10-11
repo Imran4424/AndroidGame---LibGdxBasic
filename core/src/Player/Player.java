098fd010214aefac18eebdd521588405d27a5212
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-11 23:24:26 +0600

    game physics

--- core/src/Player/Player.java
+++ core/src/Player/Player.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -34,5 +36,12 @@ public class Player extends Sprite {
 
         PolygonShape polygonShape = new PolygonShape();
         polygonShape.setAsBox(getWidth() / 2, getHeight() / 2);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = polygonShape;
+        // mass
+        fixtureDef.density = 1;
+
+        Fixture fixture = body.createFixture(fixtureDef);
     }
 }
