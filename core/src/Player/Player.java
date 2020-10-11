e5c11f8a1ff43c1dd1f329289c848fd05e1471c7
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-11 23:10:06 +0600

    game physics

--- core/src/Player/Player.java
+++ core/src/Player/Player.java
@@ -25,5 +25,6 @@ public class Player extends Sprite {
         // a static body is not affected by gravity or other forces
         // a kinematic body is not affected by gravity but it is affected by other forces
         // a dynamic body is affected by gravity and other forces
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
     }
 }
