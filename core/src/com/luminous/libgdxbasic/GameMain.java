6bf1d1e950fbf0f0b7c527eb6dc26c01b0d69118
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-14 00:25:27 +0600

    libgdx basic

--- core/src/com/luminous/libgdxbasic/GameMain.java
+++ core/src/com/luminous/libgdxbasic/GameMain.java
@@ -1,13 +1,8 @@
 package com.luminous.libgdxbasic;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import helpers.GameInfo;
 import scenes.MainMenu;
 
 public class GameMain extends Game {
